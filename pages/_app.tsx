97aa31fe0a86ad40eb8f6e0da1fe46e54ba55e22
James O'Halloran <james.p.ohalloran@gmail.com>
2022-08-26 16:29:20 -0300

    seo + footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,69 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import KofiButton from "kofi-button";
+import Head from "next/head";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <_Scripts />
+      <SEO />
       <Header />
       <Component {...pageProps} />
       <Footer />
     </>
   );
 }
+
+const _Scripts = () => (
+  <Head>
+    <script
+      async
+      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+    />
+    <script
+      dangerouslySetInnerHTML={{
+        __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+      }}
+    />
+  </Head>
+);
+
+const SEO = () => {
+  const title = "";
+  const description = "";
+  const url = "https://www.draftorders.com/";
+
+  const socialImg = {
+    url: `${url}/screenshot.png`,
+    width: 2222,
+    height: 1196,
+    alt: title,
+  };
+  return (
+    <NextSeo
+      title={title}
+      titleTemplate={title}
+      defaultTitle={title}
+      description={description}
+      canonical={url}
+      openGraph={{
+        url,
+        title: "DraftOrders.com",
+        description: "Generate your draft order, with a little fanfare.",
+        images: [socialImg],
+      }}
+    />
+  );
+};
 
 const Header = () => (
   <div className="p-2 px-4 flex bg-neutral text-white">
@@ -44,31 +97,21 @@ const Header = () => (
 );
 
 const Footer = () => (
-  <footer className="footer items-center p-4 bg-neutral text-white">
-    <div className="items-center grid-flow-col">
-      <svg
-        width={36}
-        height={36}
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
-        fillRule="evenodd"
-        clipRule="evenodd"
-        className="fill-current"
-      >
-        <path d="M22.672 15.226l-2.432.811.841 2.515c.33 1.019-.209 2.127-1.23 2.456-1.15.325-2.148-.321-2.463-1.226l-.84-2.518-5.013 1.677.84 2.517c.391 1.203-.434 2.542-1.831 2.542-.88 0-1.601-.564-1.86-1.314l-.842-2.516-2.431.809c-1.135.328-2.145-.317-2.463-1.229-.329-1.018.211-2.127 1.231-2.456l2.432-.809-1.621-4.823-2.432.808c-1.355.384-2.558-.59-2.558-1.839 0-.817.509-1.582 1.327-1.846l2.433-.809-.842-2.515c-.33-1.02.211-2.129 1.232-2.458 1.02-.329 2.13.209 2.461 1.229l.842 2.515 5.011-1.677-.839-2.517c-.403-1.238.484-2.553 1.843-2.553.819 0 1.585.509 1.85 1.326l.841 2.517 2.431-.81c1.02-.33 2.131.211 2.461 1.229.332 1.018-.21 2.126-1.23 2.456l-2.433.809 1.622 4.823 2.433-.809c1.242-.401 2.557.484 2.557 1.838 0 .819-.51 1.583-1.328 1.847m-8.992-6.428l-5.01 1.675 1.619 4.828 5.011-1.674-1.62-4.829z" />
-      </svg>
-      <p>
-        Developed by <a href="http://johalloran.dev">James OHalloran.</a>
-      </p>
-
-      <KofiButton color="#0a9396" title="Tip the dev" kofiID="S6S5EMUTW" />
-      <div className="">
-        {" "}
-        <p>Copyright © 2022 - All right reserved</p>
+  <footer className="footer flex items-center p-4 bg-neutral text-white">
+    <div>
+      <div className="flex flex-none items-center grid-flow-col space-x-4">
+        {/* <AvatarLogo /> */}
+        <div>
+          <p>
+            Developed by <a href="http://johalloran.dev">James OHalloran.</a>
+          </p>
+          <p>Copyright © 2022 - All right reserved</p>
+        </div>
+        <KofiButton color="#0a9396" title="Tip the dev" kofiID="S6S5EMUTW" />
       </div>
     </div>
-
-    <div className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+    <div className="flex-auto" />
+    <div className="flex-none">
       <a href="https://twitter.com/jamespohalloran">
         <svg
           xmlns="http://www.w3.org/2000/svg"
